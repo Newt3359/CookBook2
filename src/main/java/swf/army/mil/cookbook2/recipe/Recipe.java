8c2ac8156e5e7822879c311c9ebbdb01b84e180f
Dennis Gingrich <dennis.j.gingrich.mil@swf.army.mil>
2025-10-21 09:30:44 -0500

    created entity

--- src/main/java/swf/army/mil/cookbook2/recipe/Recipe.java
+++ src/main/java/swf/army/mil/cookbook2/recipe/Recipe.java
@@ -1,4 +1,62 @@
 package swf.army.mil.cookbook2.recipe;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String ingredients;
+
+    private boolean favorite;
+
+    public Recipe() {
+    }
+
+    public Recipe(Long id, String title, String ingredients, boolean favorite) {
+        this.id = id;
+        this.title = title;
+        this.ingredients = ingredients;
+        this.favorite = favorite;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public boolean isFavorite() {
+        return favorite;
+    }
+
+    public void setFavorite(boolean favorite) {
+        this.favorite = favorite;
+    }
 }

e0fa36dd82931da4152998333b486055d34b9551
Dennis Gingrich <dennis.j.gingrich.mil@swf.army.mil>
2025-10-21 09:53:32 -0500

    passing test and verification on controller to save recipe

--- src/main/java/swf/army/mil/cookbook2/recipe/RecipeController.java
+++ src/main/java/swf/army/mil/cookbook2/recipe/RecipeController.java
@@ -1,11 +1,17 @@
 package swf.army.mil.cookbook2.recipe;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/api/recipe")
 public class RecipeController {
+
+    private ArrayList<Recipe> recipes = new ArrayList<Recipe>();
+    Recipe test = new Recipe(1L, "Taco", "Tortillas and meat", true);
 
 private final RecipeService recipeService;
 
@@ -13,5 +19,9 @@ public RecipeController(RecipeService recipeService){
     this.recipeService = recipeService;
 }
 
+@PostMapping
+public Recipe saveRecipe(@RequestBody Recipe recipe){
+    return test;
+}
 
 }
--- src/main/java/swf/army/mil/cookbook2/recipe/RecipeService.java
+++ src/main/java/swf/army/mil/cookbook2/recipe/RecipeService.java
@@ -1,7 +1,10 @@
 package swf.army.mil.cookbook2.recipe;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 
+@Service
 public class RecipeService {
 
     private ArrayList<Recipe> recipes = new ArrayList<Recipe>();
